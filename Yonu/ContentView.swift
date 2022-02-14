213c824e31912a01975374e3e8939e5af566d132
jaimelrojo <jaime.casta@icloud.com>
2022-02-14 12:44:28 -0600

    Initial Commit

---
+++ Yonu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yonu
+//
+//  Created by Jaime Castañeda on 14/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yonu/YonuApp.swift
@@ -0,0 +1,17 @@
+//
+//  YonuApp.swift
+//  Yonu
+//
+//  Created by Jaime Castañeda on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct YonuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
